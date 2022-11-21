1b3487e2921aae03b848c2501f65561cc936ccef
HagarLaks <hagaroosh@gmail.com>
2022-11-21 21:33:12 +0200

    digits adds up for now

--- ts/application.ts
+++ ts/application.ts
@@ -6,23 +6,25 @@
 function displayButtonInfo(value) {
     alert(value);
 }
-document.getElementById("clock").onclick = function () { displayButtonInfo('clock'); };
+document.getElementById("clock").addEventListener('click', function() {displayButtonInfo(document.getElementById("clock").id)});
 document.getElementById("darkmode").onclick = function () { displayButtonInfo('darkmode'); };
 document.getElementById("root").onclick = function () { displayButtonInfo('root'); };
 document.getElementById("cloud").onclick = function () { displayButtonInfo('cloud'); };
 document.getElementById("back").onclick = function () { displayButtonInfo('back'); };
 document.getElementById("settings").onclick = function () { displayButtonInfo('settings'); };
 document.getElementById("c").onclick = function () { displayButtonInfo('c'); };
-document.getElementById("1").onclick = function () { displayButtonInfo('1'); };
-document.getElementById("2").onclick = function () { displayButtonInfo('2'); };
-document.getElementById("3").onclick = function () { displayButtonInfo('3'); };
-document.getElementById("4").onclick = function () { displayButtonInfo('4'); };
-document.getElementById("5").onclick = function () { displayButtonInfo('5'); };
-document.getElementById("6").onclick = function () { displayButtonInfo('6'); };
-document.getElementById("7").onclick = function () { displayButtonInfo('7'); };
-document.getElementById("8").onclick = function () { displayButtonInfo('8'); };
-document.getElementById("9").onclick = function () { displayButtonInfo('9'); };
-document.getElementById("0").onclick = function () { displayButtonInfo('0'); };
+
+//part one alers:
+// document.getElementById("1").onclick = function () { displayButtonInfo('1'); };
+// document.getElementById("2").onclick = function () { displayButtonInfo('2'); };
+// document.getElementById("3").onclick = function () { displayButtonInfo('3'); };
+// document.getElementById("4").onclick = function () { displayButtonInfo('4'); };
+// document.getElementById("5").onclick = function () { displayButtonInfo('5'); };
+// document.getElementById("6").onclick = function () { displayButtonInfo('6'); };
+// document.getElementById("7").onclick = function () { displayButtonInfo('7'); };
+// // document.getElementById("8").onclick = function () { displayButtonInfo('8'); };
+// document.getElementById("9").onclick = function () { displayButtonInfo('9'); };
+// document.getElementById("0").onclick = function () { displayButtonInfo('0'); };
 document.getElementById("+").onclick = function () { displayButtonInfo('+'); };
 document.getElementById("-").onclick = function () { displayButtonInfo('-'); };
 document.getElementById("/").onclick = function () { displayButtonInfo('/'); };
@@ -40,7 +42,7 @@ document.getElementById("darkmode").onclick = function () { changemode(); };
 const button = document.getElementsByClassName("button");
 const operator = document.getElementsByClassName("operator");
 const func = document.getElementsByClassName("func");
-const digits = document.getElementsByClassName("digits");
+// const digits = document.getElementsByClassName("digits");
 // for (let i = 0; i < button.length; i++) {
 //     button[i].addEventListener('click', displayButtonInfo(button));
 // }
--- ts/calculator.ts
+++ ts/calculator.ts
@@ -1,7 +1,27 @@
-let currentVal:number = 0;
+let currentVal:string = '';
 let firstOperand:number;
-let currentOperand:string;
+let currentOperand:string ='';
 let secondOperand:number;
 
 
 
+// document.getElementById("1").onclick = function () { displayButtonInfo('1'); };
+// let digits = document.querySelectorAll('.digits')
+
+// function getDigits(digit:string) {
+// currentVal += digits[]
+// }
+
+// for (let i = 0; i < digitsCol.length; i++) {digitsCol[i].addEventListener('click', curVal+digitsCol[i].id)};
+
+const digits = document.querySelectorAll('.digits');
+
+digits.forEach(digit => {
+  digit.addEventListener('click', function handleClick(event) {
+    console.log(currentVal, event);
+    currentVal = currentVal + digit.id;
+    alert(currentVal);
+    console.log(currentVal);
+    
+  });
+});
